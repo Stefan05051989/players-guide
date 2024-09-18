2e7250f1fe6534d27bcdd49b598c80f71c24b5f5
Stefan <stefan.kiers89@hotmail.com>
2024-09-18 10:15:24 +0200

    TMOAP finnished

---
+++ src/themakingsofaprogrammer/themakingsofaprogrammer.java
@@ -0,0 +1,13 @@
+package themakingsofaprogrammer;
+
+import java.util.Arrays;
+
+public class themakingsofaprogrammer {
+    public static void main(String[] args){
+        System.out.println("Hello Itvitae");
+        System.out.println("How many lines of SOP can Intellj code in total");
+        System.out.println("As much as your harddrive accepts");
+        System.out.println("oke thanks for the update");
+        System.out.println("you're welcome");
+    }
+}
