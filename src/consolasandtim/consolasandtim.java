db7ae8a41d5afd518f445afc4034f2ec17fec8ee
Stefan <stefan.kiers89@hotmail.com>
2024-09-18 12:47:06 +0200

    cosolas and tim finished

---
+++ src/consolasandtim/consolasandtim.java
@@ -0,0 +1,15 @@
+package consolasandtim;
+
+import java.util.Scanner; // importeer scanner class
+
+public class consolasandtim {
+
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in); // creeer scanner opject met deze regel
+        System.out.println("het brood is klaar");
+        System.out.println("hoeveel kost het  brood");
+        System.out.println("€2,50 per stuk");
+        System.out.println("ik wil er graag 5");
+        System.out.println("dat is goed, alstublieft");
+    }
+}
