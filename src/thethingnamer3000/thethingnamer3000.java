074030bcbdc7ada7acd6c4e5babeedd662abcaa5
Stefan <stefan.kiers89@hotmail.com>
2024-09-18 13:20:44 +0200

    The Bakstenen huis of Doom 3000

---
+++ src/thethingnamer3000/thethingnamer3000.java
@@ -0,0 +1,17 @@
+package thethingnamer3000;
+
+import java.util.Scanner;
+
+public class thethingnamer3000 {
+    public static void main(String[] args){
+            System.out.println("What kind of thing are we talking about");//reageren onderin "Run"
+    Scanner input = new Scanner(System.in);//scanner invoeren
+    String a = input.nextLine();// nextLine ipv alleen het woord next & hier staat een user input in
+    System.out.println("How would you discribe it, Big, Azure, Tattered?");
+    String b = input.nextLine();//nextLine ipv alleen het woord next
+    String c = ("Of Doom");
+    String d = "3000";
+    System.out.println("The "+ b +" "+ a + " " + c + " " + d + "!");/*ik ben hier alleen maar een heel verhaal aan het typen
+    om tot twee regels comments te komen.. Ohjah en goed op de spaties letten..*/
+    }
+}
