76744d47eb8dfb59b1275ff35154fc1589cc8682
Stefan <stefan.kiers89@hotmail.com>
2024-10-02 12:27:36 +0200

    consolas and tim refixed

--- src/consolasandtim/consolasandtim.java
+++ src/consolasandtim/consolasandtim.java
@@ -6,10 +6,9 @@ public class consolasandtim {
 
     public static void main(String[] args){
         Scanner myObj = new Scanner(System.in); // creeer scanner opject met deze regel
-        System.out.println("het brood is klaar");
-        System.out.println("hoeveel kost het  brood");
-        System.out.println("€2,50 per stuk");
-        System.out.println("ik wil er graag 5");
-        System.out.println("dat is goed, alstublieft");
+
+        System.out.println("voor wie is het brood");
+        String naam = myObj.nextLine();
+        System.out.println("voor degene naast u " + naam);
     }
 }
