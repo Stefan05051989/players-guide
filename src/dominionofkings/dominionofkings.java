6f682e710d37e97ed14f79f9e1ac2cd0a5d6d004
Stefan <stefan.kiers89@hotmail.com>
2024-11-04 15:43:05 +0100

    dominion of kings

---
+++ src/dominionofkings/dominionofkings.java
@@ -0,0 +1,20 @@
+package dominionofkings;
+import java.util.Scanner;
+
+public class dominionofkings {
+    public static void main(String[] args){
+        System.out.println("Wat is Uw Naam? :");
+        Scanner in = new Scanner(System.in);
+        String myName = in.nextLine();
+        System.out.println("Hoeveel landgoederen bezit U?");
+        int myEstate = in.nextInt();
+        in.nextLine();
+        System.out.println("Hoeveel Hertogdommen bezit U?");
+        int myDuchies = in.nextInt();
+        in.nextLine();
+        System.out.println("Hoeveel Provincies bezit U?");
+        int myProv = in.nextInt();
+        in.nextLine();
+        System.out.println(myEstate + myDuchies * 3 + myProv * 6);
+    }
+}
